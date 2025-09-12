2d7c7388f4086c2c5eaa00917c1c147e830dbddf
Marcel Scrauwen <marcel.schrauwen@vzvz.nl>
2025-09-12 07:50:21 +0200

    update

---
+++ README.md
@@ -0,0 +1,6 @@
+# Twiin enterprise architectuur werkomgeving
+Dit is een werkomgeving met een html export van architectuurmodel en bijbehorende views. Deze werkomgeving helpt enterprise-architecten van Twiin om afhankelijkheden tussen onderwerpen inzichtelijk te maken, en daarmee de samenhang en consistentie van het Twiin-afsprakenstelsel te borgen.
+
+Wees welkom om rond te klikken, vragen te stellen of zelfs feedback te geven, maar raadpleeg voor alle officiële, gevalideerde publicaties altijd de [website](https://www.twiin.nl) en de laatste release van het [afsprakenstelsel](https://www.twiin.nl/afsprakenstelsel)
+
+- [Twiin Enterprise architectuur werkomgeving](https://twiinnl.github.io/ea-werkomgeving/?view=id-9886cbc4dd6046be9bccd9ed287cbc57) (html export)
---
+++ docs/js/displayToc.js
@@ -0,0 +1,127 @@
+function toggleItem(item) {
+    var table = document.getElementById("TableGroup");
+    var i;
+
+    if (table) {
+        var height = 0;
+
+        var titles = table.getElementsByTagName('li');
+        for (i = 0; i < titles.length; i++) {
+            if (titles[i].id == item + "Title") {
+                titles[i].style.background = "#FFFFFF";
+                titles[i].style.color = "#000000";
+            } else {
+
+                titles[i].style.background = "#DDDDDD";
+                titles[i].style.color = "#666666";
+            }
+        }
+
+        var tabs = table.getElementsByClassName('ItemBody');
+        if (tabs.length > 0) {
+            height = toggleItemDetails(tabs, item);
+        }
+
+        /* table.style.height = height + aparent.clientHeight + table.clientHeight + "px";*/
+    }
+}
+function toggleItemDetails(tabs, title) {
+    var i,
+    height = 0;
+
+    for (i = 0; i < tabs.length; i++) {
+        var tab = tabs[i];
+
+        if (tab.id == title + "Table") {
+            tab.style.display = "block";
+            height = tab.clientHeight;
+        } else
+            tab.style.display = "none";
+    }
+
+    return height;
+}
+
+function bulkshow(showpage) {
+    var pagesData = document.getElementsByClassName("PageBody");
+
+    var i = 0;
+    if (showpage !== undefined) {
+        var blockdiv;
+        for (i = 0; i < pagesData.length; i++) {
+            var id = pagesData[i].attributes["id"].value;
+
+            if (showpage == id) {
+                blockdiv = document.getElementById(id);
+                blockdiv.style.display = 'block';
+
+                var divs = blockdiv.getElementsByClassName("ObjectDetailsNotes");
+                for (var j = 0; j < divs.length; j++) {
+                    var tmpStr = divs[j].innerHTML;
+                    tmpStr = tmpStr.replace(/&gt;/g, ">");
+                    tmpStr = tmpStr.replace(/&lt;/g, "<");
+
+                    tmpStr = tmpStr.replace(/#gt;/g, "&gt;");
+                    tmpStr = tmpStr.replace(/#lt;/g, "&lt;");
+
+                    divs[j].innerHTML = tmpStr;
+                }
+            } else {
+                document.getElementById(id).style.display = 'none';
+            }
+        }
+
+        if (blockdiv !== undefined) {
+            tableSel = null;
+            var tab = blockdiv.getElementsByClassName('TableGroup');
+            if (tab.length > 0) {
+                toggleItem(tab[0].getElementsByTagName('li')[0].id.replace("Title", ""), tab[0].getElementsByTagName('li')[0]);
+            }
+        }
+    }
+}
+// START - TOOLTIP CODE
+function mapRectangleMouseOver(sender) {
+
+    if (!sender || !sender.href) return;
+
+    var informationURL = sender.href;
+    if (!informationURL) return;
+
+    jQuery.get(informationURL, function (data) {
+
+        var loadedHTML = jQuery.parseHTML(data);
+        var docDOM = $('<output>').append(loadedHTML);
+        var bodyDOM = $('.ElementPage', docDOM);
+
+        if (!bodyDOM.length) return;
+
+        var itemNotes = $('.ObjectDetailsNotes', bodyDOM);
+        
+        if (!itemNotes.length) return;
+
+        var notes = unescapeHtml(itemNotes.html() || "");
+        if (notes === "") return;
+
+        var array = sender.coords.split(',');
+
+        $(".previewPanel").html("");
+        $(".previewPanel").append(notes);
+        
+        $(".previewPanel").css("margin-top", (Number(array[1]) - 100) + "px");
+        $(".previewPanel").css("margin-left", (Number(array[2]) - 400) + "px");
+        $(".previewPanel").stop(true, true).fadeIn(600); // stop oude animaties, dan fade in
+    });
+
+}
+
+function mapRectangleMouseOut(sender) {
+    if ($(".previewPanel:hover").length === 0) {
+        $(".previewPanel").stop(true, true).fadeOut(600); // stop oude animaties, dan fade out
+    }
+}
+
+function unescapeHtml(safe) {
+    return $('<div>').html(safe).text();
+}
+// EINDE - TOOLTIP CODE
